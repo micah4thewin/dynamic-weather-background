cb398a94a785016cd0f57844659dc7f19a974cbe
Micah Levason <contact@micahlevason.com>
2023-02-23 19:04:13 -0800

    debugging

--- src/js/getweatherinfo.js
+++ src/js/getweatherinfo.js
@@ -3,21 +3,19 @@ export const getWeatherInfo = async (weatherApiUrl) => {
   try {
     const response = await fetch(weatherApiUrl);
     const data = await response.json();
-    const {
-      temp_f,
-      condition
-    } = data.current;
+
+    const currentWeather = data.current;
+    const temp_f = currentWeather.temp_f;
+    const condition = currentWeather.condition.text.toLowerCase();
 
     // Set the weather information in the HTML
     const weatherInfo = document.getElementById("weather-info");
     if (weatherInfo) {
-      weatherInfo.innerHTML = `It's ${temp_f}°F and ${condition.text.toLowerCase()} outside.`;
+      weatherInfo.innerHTML = `It's ${temp_f}°F and ${condition} outside.`;
+      console.log("Weather info thing fired", `${weatherInfo.innerText}`);
     }
 
-    return {
-      temp_f,
-      condition
-    };
+    return { temp_f, condition };
   } catch (error) {
     console.error(error);
   }
